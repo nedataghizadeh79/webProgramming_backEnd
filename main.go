99f84a543d5c0822444785aa85034dac78204389
Neda.Taghizadeh <Neda.Taghizadeh@digikala.com>
2023-01-22 11:06:31 +0330

    add new files to auth_service

---
+++ main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+    "fmt"
+    "net/http"
+	"golang.org/x/crypto/bcrypt"
+)
+
+func HashPassword(password string) (string, error) {
+    bytes, err := bcrypt.GenerateFromPassword([]byte(password), 14)
+    return string(bytes), err
+}
+
+func CheckPasswordHash(password, hash string) bool {
+    err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
+    return err == nil
+}
+
+func manageHash(w http.ResponseWriter, r *http.Request) {
+    password := "secret"
+    hash, _ := HashPassword(password) // ignore error for the sake of simplicity
+
+    fmt.Println("Password:", password)
+    fmt.Println("Hash:    ", hash)
+
+    match := CheckPasswordHash(password, hash)
+    fmt.Println("Match:   ", match)
+}
+
+func main() {
+    http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+        fmt.Fprintf(w, "Hello, you've requested: %s\n", r.URL.Path)
+    })
+
+	http.HandleFunc("/signup", manageHash)
+
+    http.ListenAndServe(":80", nil)
+}
