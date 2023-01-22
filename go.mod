e9112062df1a0b6d52af70d16c7b69601f3bc0a8
Negarnbkhti42 <norob35@gmail.com>
2023-01-22 19:14:00 +0330

    Fix import issues

--- go.mod
+++ go.mod
@@ -1,9 +1,15 @@
-module example.com/greetings
+module server
 
 go 1.19
 
 require (
+	github.com/lib/pq v1.10.6
+	github.com/wpcodevo/golang-postgresql-api v0.0.0-20221002095106-8d33e5a1c688
+	golang.org/x/crypto v0.0.0-20220411220226-7b82a4e95df4
+)
+
+require (
 	github.com/go-redis/redis v6.15.9+incompatible // indirect
-	github.com/lib/pq v1.10.7 // indirect
-	golang.org/x/crypto v0.5.0 // indirect
+	github.com/golang-jwt/jwt v3.2.2+incompatible // indirect
+	github.com/google/uuid v1.3.0 // indirect
 )
--- main.go
+++ main.go
@@ -2,26 +2,36 @@ package main
  
 import (
     "net/http"
+    "os"
     "fmt"
-    "utils/utils"
+    "errors"
+    utils "server/utils"
 )
 
-func signUp(w http.ResponseWriter, r *http.Request) {}
+func signUp(w http.ResponseWriter, r *http.Request) {fmt.Println("signup")}
 
-func signIn(w http.ResponseWriter, r *http.Request) {}
+func signIn(w http.ResponseWriter, r *http.Request) {fmt.Println("signin")}
 
-func getUser(w http.ResponseWriter, r *http.Request) {}
+func getUser(w http.ResponseWriter, r *http.Request) {fmt.Println("user data")}
 
 func signOut(w http.ResponseWriter, r *http.Request) {}
 
 
 
 func main() {
-     
-    utils.connectToDb()
+    utils.ConnectToDb()
 
     http.HandleFunc("/signUp", signUp)
     http.HandleFunc("/signIn", signIn)
     http.HandleFunc("/signOut", signOut)
     http.HandleFunc("/userInfo", getUser)
+
+    err := http.ListenAndServe(":3333", nil)
+
+    if errors.Is(err, http.ErrServerClosed) {
+		fmt.Printf("server closed\n")
+	} else if err != nil {
+		fmt.Printf("error starting server: %s\n", err)
+		os.Exit(1)
+	}
 }
