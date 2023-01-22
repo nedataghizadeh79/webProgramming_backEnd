5e3f5d232dcdb9c336651c34cf527b683e35c1be
Negarnbkhti42 <norob35@gmail.com>
2023-01-22 12:00:45 +0330

    Init db connection

--- main.go
+++ main.go
@@ -1,38 +1,28 @@
 package main
-
+ 
 import (
-    "fmt"
     "net/http"
-	"golang.org/x/crypto/bcrypt"
+    "database/sql"
+    "fmt"
+    "github.com/go-redis/redis"
+    _ "github.com/lib/pq"
 )
 
-func HashPassword(password string) (string, error) {
-    bytes, err := bcrypt.GenerateFromPassword([]byte(password), 14)
-    return string(bytes), err
-}
+func signUp(w http.ResponseWriter, r *http.Request) {}
 
-func CheckPasswordHash(password, hash string) bool {
-    err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
-    return err == nil
-}
+func signIn(w http.ResponseWriter, r *http.Request) {}
 
-func manageHash(w http.ResponseWriter, r *http.Request) {
-    password := "secret"
-    hash, _ := HashPassword(password) // ignore error for the sake of simplicity
+func getUser(w http.ResponseWriter, r *http.Request) {}
 
-    fmt.Println("Password:", password)
-    fmt.Println("Hash:    ", hash)
+func signOut(w http.ResponseWriter, r *http.Request) {}
 
-    match := CheckPasswordHash(password, hash)
-    fmt.Println("Match:   ", match)
-}
+
 
 func main() {
-    http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-        fmt.Fprintf(w, "Hello, you've requested: %s\n", r.URL.Path)
-    })
+     
 
-	http.HandleFunc("/signup", manageHash)
-
-    http.ListenAndServe(":80", nil)
+    http.handleFunc("/signUp", signUp)
+    http.handleFunc("/signIn", signIn)
+    http.handleFunc("/signOut", signOut)
+    http.handleFunc("/userInfo", getUser)
 }
---
+++ utils/db.go
@@ -0,0 +1,40 @@
+package utils
+
+import(
+	"database/sql"
+    "fmt"
+    "github.com/go-redis/redis"
+    _ "github.com/lib/pq"
+)
+
+const (
+    host     = "localhost"
+    port     = 5432
+    user     = "postgres"
+    password = "example"
+    dbname   = "shaparak"
+)
+
+func connectToDb() {
+	   // connection string
+	   psqlconn := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable", host, port, user, password, dbname)
+         
+	   // open database
+   db, err := sql.Open("postgres", psqlconn)
+   CheckError(err)
+	
+	   // close database
+   defer db.Close()
+
+	   // check db
+   err = db.Ping()
+   CheckError(err)
+
+   fmt.Println("Connected!")
+}
+
+func CheckError(err error) {
+    if err != nil {
+        panic(err)
+    }
+}
