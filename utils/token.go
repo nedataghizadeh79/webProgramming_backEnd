75d6754103d6331b9b288a8b795c7f6eda8c1c6a
Negarnbkhti42 <norob35@gmail.com>
2023-01-23 11:24:00 +0330

    Change token util

--- utils/token.go
+++ utils/token.go
@@ -1,25 +1,76 @@
 package utils
 
 import (
+	"fmt"
+	"net/http"
 	"time"
 
 	"github.com/golang-jwt/jwt/v4"
 )
 
-var secret = []byte("secret-auth-token")
-
-func CreateToken() (string, error) {
+func CreateToken(privateKey string) (string, error) {
 	token := jwt.New(jwt.SigningMethodHS256)
 
 	claims := token.Claims.(jwt.MapClaims)
 	claims["exp"] = time.Now().Add(time.Hour).Unix()
 
-	tokenStr, err := token.SignedString(secret)
+	tokenStr, err := token.SignedString(privateKey)
 
 	if err != nil {
 		return "", err
 	}
 
 	return tokenStr, nil
+
+}
+
+func ValidateJWT(next func(w http.ResponseWriter, r *http.Request)) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+
+		if r.Header["Token"] != nil {
+			token, err := jwt.Parse(r.Header["Token"][0], func(t *jwt.Token) (interface{}, error) {
+				_, ok := t.Method.(*jwt.SigningMethodHMAC)
+				if !ok {
+					w.WriteHeader(http.StatusUnauthorized)
+					w.Write([]byte("not authorized"))
+				}
+				return privateKey, nil
+			})
+
+			if err != nil {
+				w.WriteHeader(http.StatusUnauthorized)
+				w.Write([]byte("not authorized: " + err.Error()))
+			}
+
+			if token.Valid {
+				next(w, r)
+			}
+		} else {
+			w.WriteHeader(http.StatusUnauthorized)
+			w.Write([]byte("not authorized"))
+		}
+	})
+}
+
+func GetJwt(w http.ResponseWriter, r *http.Request) {
+	if r.Header["Access"] != nil {
+		token, err := CreateToken("1234")
+		if err != nil {
+			return
+		}
+		fmt.Fprint(w, token)
+	}
+}
+
+func Home(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "super secret area")
+}
+
+func main() {
+
+	http.Handle("/api", ValidateJWT(Home))
+	http.HandleFunc("/jwt", GetJwt)
+
+	http.ListenAndServe(":3500", nil)
 
 }
